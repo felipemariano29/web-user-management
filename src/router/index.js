c51f7d610d1b2edcd6d24a4a215520d0ac180e72
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-29 12:37:44 -0300

    Token validation

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,33 @@ import HomeView from "../views/HomeView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import AboutView from "@/views/AboutView.vue";
 import LoginView from "@/views/LoginView.vue";
+import UsersView from "@/views/UsersView.vue";
+import axios from "axios";
+
+const AdminAuth = (to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    const req = {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    axios
+      .post("http://localhost:8686/validate", {}, req)
+      .then((res) => {
+        console.log(res.data);
+        next();
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        next("/login");
+      });
+  } else {
+    next("/login");
+  }
+};
 
 const routes = [
   {
@@ -19,6 +46,12 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginView,
+  },
+  {
+    path: "/admin/users",
+    name: "users",
+    component: UsersView,
+    beforeEnter: AdminAuth,
   },
   {
     path: "/about",
---
+++ src/views/UsersView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Painel Admin</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
